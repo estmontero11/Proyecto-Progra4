8bb783ad8ac7c46b549e9e273025dafbdc83bb86
Daniel Gutierrez <daniguti14@gmail.com>
2017-10-06 19:30:59 -0600

    Pruebas y SQL

--- DED Drivers/src/java/cr/ac/una/progra4/deddrivers/bl/UsuarioBL.java
+++ DED Drivers/src/java/cr/ac/una/progra4/deddrivers/bl/UsuarioBL.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -44,49 +43,3 @@ public class UsuarioBL extends BaseBL implements IBaseBL<Usuario, Integer>{
         return this.getDao(className).findAll();
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cr.ac.una.progra4.deddrivers.bl;
-
-import cr.ac.una.progra4.deddrivers.domain.Usuario;
-import java.util.List;
-
-/**
- *
- * @author esteban
- */
-public class UsuarioBL extends BaseBL implements IBaseBL<Usuario, Integer> {
-    public UsuarioBL(){
-    super();
-    }
-    @Override
-    public void save(Usuario o) {
-    this.getDao(o.getClass().getName()).save(o);    
-    }
-
-    @Override
-    public Usuario merge(Usuario o) {
-        return (Usuario) this.getDao(o.getClass().getName()).merge(o);
-    }
-
-    @Override
-    public void delete(Usuario o) {
-        this.getDao(o.getClass().getName()).delete(o);
-    }
-
-    @Override
-    public Usuario findById(Integer o) {
-        return (Usuario) this.getDao(o.getClass().getName()).findById(o);
-    }
-
-    @Override
-    public List<Usuario> findAll(String className) {
-        return this.getDao(className).findAll();
-    }
-    
-}
->>>>>>> 1408d49c7b344e8a113f9f86d6bf331768fbed59
